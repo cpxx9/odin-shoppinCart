2cb7afe9e4537717362a699c606557b56a650e98
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-09-25 19:41:03 -0400

    refactor: pull router config out of App component for better reusability

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,14 @@ import './styles/App.css';
 
 import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
 
+import routesConfig from './routes/router';
+
 import MainLayout from './layouts/MainLayout';
 import Root from './routes/Root';
 import Store from './routes/Store';
 import NotFound from './routes/NotFound';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        path: '/',
-        element: <Root />,
-      },
-      {
-        path: '/store',
-        element: <Store />,
-      },
-      {
-        path: '*',
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(routesConfig);
 
 function App() {
   return <RouterProvider router={router} />;
