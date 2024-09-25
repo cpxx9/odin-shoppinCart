df0eec51a93b2a9e184acd89cb8684c75003eae2
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-09-24 21:00:42 -0400

    set up basic structure and routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import './styles/App.css';
 
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
+import Root from './routes/Root';
+import Store from './routes/Store';
+import NotFound from './routes/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        path: '/',
+        element: <Root />,
+      },
+      {
+        path: '/store',
+        element: <Store />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
