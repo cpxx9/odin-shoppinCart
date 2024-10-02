33d4c44f55be0437fc32e88662babcb9e5dcf524
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-10-02 07:45:27 -0400

    test for different props

--- src/_tests_/Listing.test.jsx
+++ src/_tests_/Listing.test.jsx
@@ -11,9 +11,21 @@ const item = {
   description:
     'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
   category: "men's clothing",
-  image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
   rating: {
     rate: 3.9,
+    count: 120,
+  },
+};
+
+const item1 = {
+  id: 2,
+  title: 'Fjaraven - Foldsack No. 5 Bkpack, Fits 4 Laptops',
+  price: 89.95,
+  description:
+    'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+  category: "women's clothing",
+  rating: {
+    rate: 2.8,
     count: 120,
   },
 };
@@ -61,5 +73,22 @@ describe('Item Component', () => {
     await user.click(button);
 
     expect(screen.getByText(shortDescription)).toBeInTheDocument;
+  });
+
+  it('works with different props', () => {
+    render(<Listing item={item1} />);
+
+    const title = screen.getByRole('heading', { name: item1.title });
+    const category = screen.getByRole('heading', { name: item1.category });
+    const description = screen.getByText(shortDescription);
+    const rating = screen.getByRole('heading', {
+      name: item1.rating.rate + ' / ' + item1.rating.count,
+    });
+    const price = screen.getByRole('heading', { name: item1.price });
+    expect(title).toBeInTheDocument();
+    expect(category).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(rating).toBeInTheDocument();
+    expect(price).toBeInTheDocument();
   });
 });
