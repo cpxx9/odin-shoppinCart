4a58ce8a8dad363285d30ec8b4312ed48826b4a4
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-10-02 08:08:25 -0400

    feat: api call for fake shop

--- src/components/Listings/Listings.jsx
+++ src/components/Listings/Listings.jsx
@@ -5,7 +5,25 @@ import PropTypes from 'prop-types';
 import { RingLoader } from 'react-spinners';
 
 const Listings = ({ isHome = false }) => {
-  return <div>Listings</div>;
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const apiUrl = `https://fakestoreapi.com/products${isHome ? '?limit=3' : ''}`;
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        console.log(data);
+        setItems(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchItems();
+  }, []);
 };
 
 Listings.propTypes = {
