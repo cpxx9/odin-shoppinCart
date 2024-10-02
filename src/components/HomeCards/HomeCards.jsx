fe822eee1625eafa869797f9919bcb08f7dd92d9
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-10-01 20:53:21 -0400

    feat: render links to other pages in the cards

--- src/components/HomeCards/HomeCards.jsx
+++ src/components/HomeCards/HomeCards.jsx
@@ -1,9 +1,24 @@
 import Card from '../card/Card';
+import { Link } from 'react-router-dom';
 
 const HomeCards = () => {
   return (
     <section>
-      <Card />
+      <Card>
+        <h2>Store</h2>
+        <p>
+          Browse our collection of PC&apos;s and start being more productive
+          today
+        </p>
+        <Link to={'/store'}>Browse PC&apos;s</Link>
+      </Card>
+      <Card>
+        <h2>Community</h2>
+        <p>
+          Take a look at the community for helpful tips, or a place to chat!
+        </p>
+        <Link to={'/community'}>Explore</Link>
+      </Card>
     </section>
   );
 };
