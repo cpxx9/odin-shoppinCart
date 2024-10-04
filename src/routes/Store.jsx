75feb28f994ff9256a41646684c8741c5d1ef502
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-10-04 14:21:24 -0400

    fix: cart works on store page

--- src/routes/Store.jsx
+++ src/routes/Store.jsx
@@ -1,7 +1,10 @@
+import { useOutletContext } from 'react-router-dom';
 import Listings from '../components/Listings/Listings';
 
 const Store = () => {
-  return <Listings />;
+  const [cart, setCart] = useOutletContext();
+
+  return <Listings cart={cart} setCart={setCart} />;
 };
 
 export default Store;
