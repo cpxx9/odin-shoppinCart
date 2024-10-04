552f39e74e4c4c6b68652292173925bc04ed93ee
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-10-04 14:14:07 -0400

    style: styled nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,24 +2,27 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Cart from '../Cart/Cart';
 import { NavLink } from 'react-router-dom';
+import StyledNav from './StyledNav';
 
 const Nav = ({ cart = [], setCart = () => {} }) => {
   const linkClass = ({ isActive }) => (isActive ? 'active-link' : '');
 
   return (
-    <nav>
+    <StyledNav>
       <NavLink to={'/'}>
         <img src="" alt="" />
         <h2>Odin Storefront</h2>
       </NavLink>
-      <NavLink to={'/'} className={linkClass}>
-        Home
-      </NavLink>
-      <NavLink to={'/store'} className={linkClass}>
-        Store
-      </NavLink>
-      <Cart cart={cart} setCart={setCart} />
-    </nav>
+      <div>
+        <NavLink to={'/'} className={linkClass}>
+          Home
+        </NavLink>
+        <NavLink to={'/store'} className={linkClass}>
+          Store
+        </NavLink>
+        <Cart cart={cart} setCart={setCart} />
+      </div>
+    </StyledNav>
   );
 };
 
